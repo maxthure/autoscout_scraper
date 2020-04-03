d3a7d0ec41de99ec5c5030c2785818575f024806
maxthure <th.nebendahl@gmail.com>
2020-04-03 17:35:14 +0200

    First attempt to implement a Sqlite db
    Moved scraped.sqlite to venv/scraped.sqlite and had to change the gitignore
    Tried to use pandas to add the DataFrame to the db but get this error:
    __init__() missing 1 required positional argument: 'item_type'

--- venv/main.py
+++ venv/main.py
@@ -7,7 +7,7 @@ import re  # regular expressions
 import os  # Dateipfade erstellen und lesen
 import pandas as pd  # Datenanalyse und -manipulation
 from pathlib import Path
-
+import sqlalchemy
 
 
 folders = [Path("out/data/visited/"), Path("out/data/autos/")]
@@ -45,22 +45,22 @@ while True:
         car_URLs = []
         car_URLs_unique = []
 
-        for page in range(1, 21):
+        for page in range(1, 2):
             try:
-                url = 'https://www.autoscout24.de/lst/?sort=age&desc=1&ustate=N%2CU&size=20&page=' + str(page) + '&cy=' + countries[country] + '&atype=C&'
+                url = 'https://www.autoscout24.de/lst/?sort=age&desc=1&ustate=N%2CU&size=5&page=' + str(page) + '&cy=' + countries[country] + '&atype=C&'
                 only_a_tags = SoupStrainer("a")
                 soup = BeautifulSoup(urllib.request.urlopen(url).read(), 'lxml', parse_only=only_a_tags)
                 for link in soup.find_all("a"):
                     if r"/angebote/" in str(link.get("href")):
                         car_URLs.append(link.get("href"))
 
+                car_URLs_unique = [car for car in list(set(car_URLs)) if car not in visited_urls]
                 print(f'Lauf {cycle_counter} | {country} | Seite {page} | {len(car_URLs_unique)} neue URLs')
 
             except Exception as e:
                 print("Übersicht: " + str(e) + " " * 50)
                 pass
 
-            car_URLs_unique = [car for car in list(set(car_URLs)) if car not in visited_urls]
 
         if len(car_URLs_unique) > 0:
             for URL in car_URLs_unique:
@@ -110,6 +110,15 @@ while True:
         df = pd.DataFrame(multiple_cars_dict).T
         df.to_csv("out/data/autos/" + re.sub("[.,:,-, ]", "_", str(datetime.now())) + ".csv", sep=";",
                   index_label="url")
+        try:
+            engine = sqlalchemy.create_engine('sqlite:///scraped.sqlite')
+            df.to_sql(name='autos',con=engine, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None, dtype={'price':sqlalchemy.types.INTEGER,
+                                                                                                                                      'ausstattung_liste':sqlalchemy.types.ARRAY}, method=None)
+            print("DataFrame saved in DB")
+        except Exception as error:
+            print(error.__cause__)
+            print("Error while connecting to sqlite: ", error)
+
     else:
         print("Keine Daten")
     with open("out/data/visited/visited_urls.json", "w") as file:

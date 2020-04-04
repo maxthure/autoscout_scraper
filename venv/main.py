f9635db1cd8492a24429f1e9c4155aed6e337ffc
maxthure <th.nebendahl@gmail.com>
2020-04-04 19:57:28 +0200

    Cleaned code by adding a main function and dividing the code into different functions

--- venv/main.py
+++ venv/main.py
@@ -9,32 +9,45 @@ import pandas as pd  # Datenanalyse und -manipulation
 from pathlib import Path
 import sqlalchemy
 
-
 folders = [Path("out/data/visited/"), Path("out/data/autos/")]
-
-for folder in folders:
-    if not os.path.isdir(folder):
-        os.makedirs(folder)
-        print(folder, "erstellt.")
-    else:
-        print(folder, "existiert bereits")
-
 path_to_visited_urls = "out/data/visited/visited_urls.json"
 
-if not os.path.isfile(path_to_visited_urls):
-    with open(path_to_visited_urls, "w") as file:
-        json.dump([], file)
 
-countries = {"Deutschland": "D"}
+def main():
+    create_folders()
+    create_path_to_visited_urls()
+    multiple_cars_dict = scrape_autoscout()
+    save_cars(multiple_cars_dict)
 
-car_counter = 1
-cycle_counter = 0
-filterset = {"url", "country", "date", "Fahrzeughalter", "HU/AU neu", "Garantie", "Scheckheftgepflegt",
-             "Nichtraucherfahrzeug", "Marke", "Modell", "Angebotsnummer", "Erstzulassung",
-             "Außenfarbe", "Lackierung", "Farbe laut Hersteller", "Innenausstattung", "Karosserieform", "Anzahl Türen", "Sitzplätze",
-             "Schlüsselnummer", "Getriebeart", "Hubraum", "Anstriebsart", "Kraftstoff", "Schadstoffklasse","Feinstaubplakette", "Leistung",
-             "Kilometerstand", "Getriebe", "Kraftstoffverbr.*", "Ausstattung", "ort", "price", "HU Prüfung"}
-while True:
+
+def create_folders():
+    for folder in folders:
+        if not os.path.isdir(folder):
+            os.makedirs(folder)
+            print(folder, "erstellt.")
+        else:
+            print(folder, "existiert bereits")
+
+
+def create_path_to_visited_urls():
+    if not os.path.isfile(path_to_visited_urls):
+        with open(path_to_visited_urls, "w") as file:
+            json.dump([], file)
+
+
+def scrape_autoscout():
+    countries = {"Deutschland": "D"}
+
+    car_counter = 1
+    cycle_counter = 0
+    filterset = {"url", "country", "date", "Fahrzeughalter", "HU/AU neu", "Garantie", "Scheckheftgepflegt",
+                 "Nichtraucherfahrzeug", "Marke", "Modell", "Angebotsnummer", "Erstzulassung",
+                 "Außenfarbe", "Lackierung", "Farbe laut Hersteller", "Innenausstattung", "Karosserieform",
+                 "Anzahl Türen", "Sitzplätze",
+                 "Schlüsselnummer", "Getriebeart", "Hubraum", "Anstriebsart", "Kraftstoff", "Schadstoffklasse",
+                 "Feinstaubplakette", "Leistung",
+                 "Kilometerstand", "Getriebe", "Kraftstoffverbr.*", "Ausstattung", "ort", "price", "HU Prüfung"}
+
     with open(path_to_visited_urls) as file:
         visited_urls = json.load(file)
 
@@ -49,12 +62,12 @@ while True:
 
         car_URLs = []
         car_URLs_unique = []
-        #Suche nach Hersteller + Modell
+        # Suche nach Hersteller + Modell
         make = 'BMW'
         model = '120'
         for page in range(1, 2):
             try:
-                url = 'https://www.autoscout24.de/lst/'+make+'/' + model +'?sort=age&desc=1&ustate=N%2CU&size=20&page=' \
+                url = 'https://www.autoscout24.de/lst/' + make + '/' + model + '?sort=age&desc=1&ustate=N%2CU&size=20&page=' \
                       + str(page) + '&cy=' + countries[country] + '&atype=C&'
                 only_a_tags = SoupStrainer("a")
                 soup = BeautifulSoup(urllib.request.urlopen(url).read(), 'lxml', parse_only=only_a_tags)
@@ -69,7 +82,6 @@ while True:
                 print("Übersicht: " + str(e) + " " * 50)
                 pass
 
-
         if len(car_URLs_unique) > 0:
             for URL in car_URLs_unique:
                 print(f'Lauf {cycle_counter} | {country} | Auto {car_counter}' + ' ' * 50)
@@ -81,7 +93,7 @@ while True:
                     car = BeautifulSoup(urllib.request.urlopen('https://www.autoscout24.de' + URL).read(), 'lxml')
 
                     for key, value in zip(car.find_all("dt"), car.find_all("dd")):
-                       # print(key)
+                        # print(key)
                         if key.text.replace("\n", "") in filterset:
                             car_dict[key.text.replace("\n", "")] = value.text.replace("\n", "")
                     car_dict["haendler"] = car.find("div", attrs={"class": "cldt-vendor-contact-box",
@@ -89,7 +101,7 @@ while True:
                     car_dict["ort"] = car.find("div", attrs={"class": "sc-grid-col-12",
                                                              "data-item-name": "vendor-contact-city"}).text
 
-                    car_dict["price"] =\
+                    car_dict["price"] = \
                         "".join(re.findall(r'[0-9]+', car.find("div", attrs={"class": "cldt-price"}).text))
 
                     ausstattung = []
@@ -101,7 +113,7 @@ while True:
                     for element in list(set(ausstattung)):
                         austattung_liste = element.split("\n")
                         ausstattung2.extend(austattung_liste)
-                    #car_dict["ausstattung_liste"] = sorted(list(set(ausstattung2)))
+                    # car_dict["ausstattung_liste"] = sorted(list(set(ausstattung2)))
                     multiple_cars_dict[URL] = car_dict
                     visited_urls.append(URL)
                     print(car_dict)
@@ -114,19 +126,28 @@ while True:
             print("\U0001F634")
             sleep(60)
 
+    with open("out/data/visited/visited_urls.json", "w") as file:
+            json.dump(visited_urls, file)
+    return multiple_cars_dict
+
+
+def save_cars(multiple_cars_dict):
     if len(multiple_cars_dict) > 0:
         df = pd.DataFrame(multiple_cars_dict).T
         df.to_csv("out/data/autos/" + re.sub("[.,:,-, ]", "_", str(datetime.now())) + ".csv", sep=";",
                   index_label="url")
         try:
             engine = sqlalchemy.create_engine('sqlite:///scraped.sqlite')
-            df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')','')
-            df.to_sql('autos',engine, None, 'replace', True, None, None, None, None)
+            df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')',
+                                                                                                                   '')
+            df.to_sql('autos', engine, None, 'replace', True, None, None, None, None)
             print("DataFrame saved in DB")
         except Exception as error:
             print("Error while connecting to sqlite: ", error)
 
     else:
         print("Keine Daten")
-    with open("out/data/visited/visited_urls.json", "w") as file:
-        json.dump(visited_urls, file)
+
+
+if __name__ == "__main__":
+    main()
